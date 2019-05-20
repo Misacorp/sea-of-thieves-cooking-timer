86e3c3aab8b7f70d71fe292780119e137e0adef2
Misa Jokisalo <misa.jokisalo@joinex.com>
2019-05-20 17:39:28 +0300

    In-progress work on USER_LEFT event handling on the server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,44 @@
 const server = require("http").createServer();
 const io = require("socket.io")(server);
 
+// This crashes if client is in no rooms
+const getClientRooms = clientId => Object.entries(io.sockets.adapter.sids[clientId]); // Returns [[key, isClientInThisRoom], ...]
+
+// Emit a "Client left room" event to everyone in a specific room.
+const emitLeaveRoom = (roomId, nickname) => {
+  console.log(`Client is leaving ${roomId} and joining ${roomId || 'nothing'}`);
+  io.to(roomId).emit("USER_LEFT", { nickname, timestamp: new Date() });
+}
+
 io.on("connection", client => {
-  console.log('Clients:', io.sockets.clients());
+  const clients = io.sockets.clients();
+  // console.log('Clients:', clients);
 
   /**
    * Assign client to a room on their request.
    * Notify room members of the new client.
    */
-  client.on('JOIN_ROOM', data => {
+  client.on("JOIN_ROOM", data => {
     const { nickname, roomCode } = data;
+    const currentRooms = getClientRooms(client.id);
 
-    console.log(`${nickname} wants to join room ${roomCode}`);
+    // Leave any rooms the client is currently in
+    client.leaveAll();
+
+    // Loop through all rooms the client is connected to
+    for (let i = 0; i < currentRooms.length; i += 1) {
+      // Emit an event saying the user left
+      const roomId = currentRooms[i][0];
+      emitLeaveRoom(roomId, nickname);
+    }
+
+    // Join new room and broadcast joined event.
     client.join(roomCode);
-    io.to(roomCode).emit('USER_JOINED', { nickname, timestamp: new Date() });
+    io.to(roomCode).emit("USER_JOINED", { nickname, timestamp: new Date() });
   });
 
-  client.on('CREATE_ROOM', data => {
-    console.log('Client wants to CREATE room', data);
+  client.on("CREATE_ROOM", data => {
+    console.log("Client wants to CREATE room", data);
   });
 
   /**
@@ -39,6 +60,13 @@ io.on("connection", client => {
   });
 
   client.on("disconnect", () => {
+    // Loop through all rooms the client is connected to
+    const currentRooms = getClientRooms(client.id);
+    for (let i = 0; i < currentRooms.length; i += 1) {
+      // Emit an event saying the user left
+      const roomId = currentRooms[i][0];
+      emitLeaveRoom(roomId, client.id);
+    }
     console.log("Client disconnected");
   });
 });
