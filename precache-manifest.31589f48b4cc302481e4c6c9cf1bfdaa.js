e579326b84d5ac6d9b568e1412a943f78f583df8
Misacorp <misa.jokisalo@gmail.com>
2019-08-17 11:32:22 +0300

    Updates

---
+++ precache-manifest.31589f48b4cc302481e4c6c9cf1bfdaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c77d1c6fa2b49c3e46f2de72c033f7",
+    "url": "/sea-of-thieves-cooking-timer/index.html"
+  },
+  {
+    "revision": "b1aaf17d6360ca6785e4",
+    "url": "/sea-of-thieves-cooking-timer/static/css/main.036f711b.chunk.css"
+  },
+  {
+    "revision": "23982f2617aa3563b2b2",
+    "url": "/sea-of-thieves-cooking-timer/static/js/2.aaec88a2.chunk.js"
+  },
+  {
+    "revision": "b1aaf17d6360ca6785e4",
+    "url": "/sea-of-thieves-cooking-timer/static/js/main.9554e31a.chunk.js"
+  },
+  {
+    "revision": "9dc4d7cde6b28a05b705",
+    "url": "/sea-of-thieves-cooking-timer/static/js/runtime~main.d65499ee.js"
+  },
+  {
+    "revision": "7c0e3fdb937751432016116ef04c8010",
+    "url": "/sea-of-thieves-cooking-timer/static/media/annoying-vuvuzela-tone.7c0e3fdb.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sea-of-thieves-cooking-timer/precache-manifest.778d6eeab5b1250be8424fb0a94c5c6e.js"
+  "/sea-of-thieves-cooking-timer/precache-manifest.31589f48b4cc302481e4c6c9cf1bfdaa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sea-of-thieves-cooking-timer/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
